414887d6fd2e9538213482da74e4af87452429bf
aman <bora.amanalambora@gmail.com>
2020-05-12 18:03:09 +0530

    add-bootstrap

--- django-setup/alwayz/frontend/src/components/login/login.component.ts
+++ django-setup/alwayz/frontend/src/components/login/login.component.ts
@@ -23,4 +23,8 @@ export class LoginComponent implements OnInit {
     );
   }
 
+  signup(){
+    this.router.navigate(['signup']);
+  }
+
 }
--- django-setup/alwayz/frontend/src/components/song/song.component.ts
+++ django-setup/alwayz/frontend/src/components/song/song.component.ts
@@ -23,7 +23,7 @@ export class SongComponent implements OnInit {
   }
 
   liked(){
-    
+
   }
 
   logout(){
